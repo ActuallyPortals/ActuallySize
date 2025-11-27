8f86ce3e67762d9ce7be728bd9a39108e66f804e
ActuallyPortals <olisqueaseis@gmail.com>
2025-11-27 03:17:40 -0500

    Fixes Clast Cast problem with Follow Owner goal

--- src/main/java/actually/portals/ActuallySize/mixin/FollowOwnerGoalMixin.java
+++ src/main/java/actually/portals/ActuallySize/mixin/FollowOwnerGoalMixin.java
@@ -3,18 +3,23 @@ package actually.portals.ActuallySize.mixin;
 import actually.portals.ActuallySize.pickup.mixininterfaces.EntityDualityCounterpart;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.goal.FollowOwnerGoal;
 import net.minecraft.world.entity.ai.goal.Goal;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(FollowOwnerGoal.class)
 public abstract class FollowOwnerGoalMixin extends Goal {
+
+    @Shadow @Final private TamableAnimal tamable;
 
     @WrapMethod(method = "unableToMove")
     public boolean onTryMoving(Operation<Boolean> original) {
 
         // If held, no move
-        EntityDualityCounterpart dualityEntity = (EntityDualityCounterpart) this;
+        EntityDualityCounterpart dualityEntity = (EntityDualityCounterpart) this.tamable;
         if (dualityEntity.actuallysize$isHeld()) { return false; }
 
         // Not held not our business
