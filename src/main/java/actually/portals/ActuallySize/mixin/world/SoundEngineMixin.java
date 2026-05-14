755b44f3595c4595362ec406315e8db03a8e1b32
ActuallyPortals <olisqueaseis@gmail.com>
2026-05-13 23:20:38 -0400

    Allows giants to hear further away better

---
+++ src/main/java/actually/portals/ActuallySize/mixin/world/SoundEngineMixin.java
@@ -0,0 +1,32 @@
+package actually.portals.ActuallySize.mixin.world;
+
+import actually.portals.ActuallySize.ASIUtilities;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import gunging.ootilities.GungingOotilitiesMod.ootilityception.OotilityNumbers;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SoundEngine.class)
+public class SoundEngineMixin {
+
+    @WrapOperation(method = "play", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/sounds/Sound;getAttenuationDistance()I"))
+    private int WithGiantHearing(Sound instance, Operation<Integer> original) {
+
+        /*
+         * Sound attenuation at 16 blocks is too little for giants.
+         */
+        int usual = original.call(instance);
+
+        // Giants have better hearing
+        Player localPlayer = Minecraft.getInstance().player;
+        if (localPlayer != null) { usual = OotilityNumbers.ceil(ASIUtilities.getEntityScale(localPlayer) * usual);}
+
+        // Attenuate properly
+        return usual;
+    }
+}
